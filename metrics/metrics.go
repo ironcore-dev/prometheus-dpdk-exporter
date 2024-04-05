3402242e47e918e506dfe1b34956f0fa64fb435e
Viliam Lorinc <viliam.lorinc@t-systems.com>
2024-04-05 15:38:33 +0200

    change log format

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -4,19 +4,20 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net"
 	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sirupsen/logrus"
 )
 
 var SocketPath = "/var/run/dpdk/rte/dpdk_telemetry.v2"
 
-func QueryTelemetry(conn net.Conn, command string, response interface{}) {
+func queryTelemetry(conn net.Conn, log *logrus.Logger, command string, response interface{}) {
 	_, err := conn.Write([]byte(command))
 	if err != nil {
-		log.Fatalf("Failed to send command to %s: %v", SocketPath, err)
+		log.Errorf("Failed to send command to %s: %v", SocketPath, err)
+		return
 	}
 
 	respBytes := make([]byte, 1024*6)
@@ -24,7 +25,8 @@ func QueryTelemetry(conn net.Conn, command string, response interface{}) {
 	for {
 		n, err := conn.Read(respBytes)
 		if err != nil {
-			log.Fatalf("Failed to read response from %s: %v", SocketPath, err)
+			log.Errorf("Failed to read response from %s: %v", SocketPath, err)
+			return
 		}
 		responseBuffer.Write(respBytes[:n])
 		parts := strings.SplitN(command, ",", 2)
@@ -36,45 +38,39 @@ func QueryTelemetry(conn net.Conn, command string, response interface{}) {
 
 	err = json.Unmarshal(responseBuffer.Bytes(), response)
 	if err != nil {
-		log.Fatalf("Failed to unmarshal JSON response: %v", err)
+		log.Errorf("Failed to unmarshal JSON response: %v", err)
 	}
 }
 
-func Update(conn net.Conn, hostname string) {
+func Update(conn net.Conn, hostname string, log *logrus.Logger) {
 	var ethdevList EthdevList
-	QueryTelemetry(conn, "/ethdev/list", &ethdevList)
-	//log.Println("ethdevList", ethdevList)
+	queryTelemetry(conn, log, "/ethdev/list", &ethdevList)
 
 	for _, id := range ethdevList.Value {
 		var ethdevInfo EthdevInfo
-		QueryTelemetry(conn, fmt.Sprintf("/ethdev/info,%d", id), &ethdevInfo)
-		//log.Println("ethdevInfo", ethdevInfo)
+		queryTelemetry(conn, log, fmt.Sprintf("/ethdev/info,%d", id), &ethdevInfo)
 
 		var ethdevXstats EthdevXstats
-		QueryTelemetry(conn, fmt.Sprintf("/ethdev/xstats,%d", id), &ethdevXstats)
-		//log.Println("ethdevXstats", ethdevXstats)
+		queryTelemetry(conn, log, fmt.Sprintf("/ethdev/xstats,%d", id), &ethdevXstats)
 
 		for statName, statValueFloat := range ethdevXstats.Value {
 			InterfaceStat.With(prometheus.Labels{"interface": ethdevInfo.Value.Name, "stat_name": statName}).Set(statValueFloat)
 		}
 	}
 	var dpserviceNatPort DpServiceNatPort
-	QueryTelemetry(conn, "/dp_service/nat/used_port_count", &dpserviceNatPort)
-	//log.Println("Dpservice nat port", dpserviceNatPort)
+	queryTelemetry(conn, log, "/dp_service/nat/used_port_count", &dpserviceNatPort)
 	for ifName, portCount := range dpserviceNatPort.Value {
 		InterfaceStat.With(prometheus.Labels{"interface": ifName, "stat_name": "nat_used_port_count"}).Set(float64(portCount))
 	}
 
 	var dpserviceVirtsvcPort DpServiceVirtsvcPort
-	QueryTelemetry(conn, "/dp_service/virtsvc/used_port_count", &dpserviceVirtsvcPort)
-	//log.Println("Dpservice virtsvc port", dpserviceVirtsvcPort)
+	queryTelemetry(conn, log, "/dp_service/virtsvc/used_port_count", &dpserviceVirtsvcPort)
 	for ifName, portCount := range dpserviceVirtsvcPort.Value {
 		InterfaceStat.With(prometheus.Labels{"interface": ifName, "stat_name": "virtsvc_used_port_count"}).Set(float64(portCount))
 	}
 
 	var dpserviceCallCount DpServiceGraphCallCount
-	QueryTelemetry(conn, "/dp_service/graph/call_count", &dpserviceCallCount)
-	//log.Println("dpserviceCallCount", dpserviceCallCount)
+	queryTelemetry(conn, log, "/dp_service/graph/call_count", &dpserviceCallCount)
 
 	for graphNodeName, callCount := range dpserviceCallCount.GraphCallCnt.Node_0_to_255 {
 		CallCount.With(prometheus.Labels{"node_name": hostname, "graph_node": graphNodeName}).Set(callCount)
